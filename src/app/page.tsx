62e77047a01fb6e86379bbc5bcf1e639f771318d
mabinhang2021 <1223841837@qq.com>
2025-08-14 22:35:55 +0800

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button"
 
-const Page = ()=>{
+import { Prisma } from "@/generated/prisma"
+import { prisma } from "@/lib/db"
 
+const Page = async ()=>{
+  const users = await prisma.user.findMany()
   return (
     <div className="font-bold text-rose-500">
       <Button>
         click me 
         </Button>
+        {JSON.stringify(users)}
     </div>
   )
 }
